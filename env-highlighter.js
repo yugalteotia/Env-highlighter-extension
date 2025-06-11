58819167a58d068d0699b097ddf005f012326665
Yugal Teotia <67860100+yugalteotia@users.noreply.github.com>
2025-06-11 14:31:33 +0530

    Add files via upload

--- env-highlighter.js
+++ env-highlighter.js
@@ -19,20 +19,61 @@
         label = "DEVELOPMENT";
     }
 
+    function adjustColor(color, amount) {
+        return "#" + color.replace(/^#/, "").replace(/../g, color =>
+            ("0" + Math.min(255, Math.max(0, parseInt(color, 16) + amount)).toString(16)).slice(-2)
+        );
+    }
+
+    function addEnvLabel() {
+        if (document.getElementById("env-label")) return;
+
+        const marker = document.createElement("div");
+        marker.id = "env-label";
+
+        marker.innerHTML = label.split('').join('<br>');
+        const labelBgColor = adjustColor(bgColor, -30);
+
+        Object.assign(marker.style, {
+            position: "fixed",
+            left: "8px",
+            top: "35%",
+            zIndex: "9999",
+            padding: "10px",
+            backgroundColor: labelBgColor,
+            color: "#ffffff",
+            fontWeight: "bold",
+            fontSize: "30px",
+            lineHeight: "1.2",
+            borderTopRightRadius: "8px",
+            borderTopLeftRadius: "8px",
+            borderBottomRightRadius: "8px",
+            borderBottomLeftRadius: "8px",
+            boxShadow: "2px 2px 6px rgba(0, 0, 0, 0.2)",
+            textAlign: "center",
+            pointerEvents: "none",
+            whiteSpace: "pre-line"
+        });
+
+        document.body.appendChild(marker);
+    }
 
 
     function applyStyles() {
         const menuBar = document.querySelector('.vertical-main-menu-bar');
         if (menuBar) {
             menuBar.style.backgroundColor = bgColor;
+            addEnvLabel();
+
             if (shouldBlink) {
                 let blinkState = true;
                 setInterval(() => {
                     document.title = blinkState ? "⚠️ PRODUCTION ⚠️" : "🚨🚨 CAREFUL 🚨🚨";
                     blinkState = !blinkState;
                 }, 1000);
-            } else
+            } else {
                 document.title = `⚠️ ${label} ⚠️`;
+            }
 
             return true;
         }
